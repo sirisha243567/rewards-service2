5dbd910338c1c731f9334f75beb72115834b658a
sirisha <sirisha.nachimuthu@gmail.com>
2020-07-13 00:08:52 -0500

    First Commit

---
+++ src/main/java/com/customer/rewards/RewardsService2Application.java
@@ -0,0 +1,13 @@
+package com.customer.rewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardsService2Application.class, args);
+	}
+
+}
