22cd8e97a6d2fd3b32f6f0601d64f66ecfaa94bc
sirisha <sirisha.nachimuthu@gmail.com>
2020-07-13 00:47:58 -0500

    Added model classes

---
+++ src/main/java/com/customer/rewards/model/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.customer.rewards.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+
+	@JsonProperty("errorMessage")
+	private String errorMessage;
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+}
---
+++ src/main/java/com/customer/rewards/model/RewardsResponse.java
@@ -0,0 +1,62 @@
+package com.customer.rewards.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class RewardsResponse {
+
+	@JsonProperty("customerId")
+	private long customerId;
+	@JsonProperty("lastMonthRewardPoints")
+	private long lastMonthRewardPoints;
+	@JsonProperty("lastSecondMonthRewardPoints")
+	private long lastSecondMonthRewardPoints;
+	@JsonProperty("lastThirdMonthRewardPoints")
+	private long lastThirdMonthRewardPoints;
+	@JsonProperty("totalRewards")
+	private long totalRewards;
+
+	public long getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(long customerId) {
+		this.customerId = customerId;
+	}
+
+	public long getLastMonthRewardPoints() {
+		return lastMonthRewardPoints;
+	}
+
+	public void setLastMonthRewardPoints(long lastMonthRewardPoints) {
+		this.lastMonthRewardPoints = lastMonthRewardPoints;
+	}
+
+	public long getLastSecondMonthRewardPoints() {
+		return lastSecondMonthRewardPoints;
+	}
+
+	public void setLastSecondMonthRewardPoints(long lastSecondMonthRewardPoints) {
+		this.lastSecondMonthRewardPoints = lastSecondMonthRewardPoints;
+	}
+
+	public long getLastThirdMonthRewardPoints() {
+		return lastThirdMonthRewardPoints;
+	}
+
+	public void setLastThirdMonthRewardPoints(long lastThirdMonthRewardPoints) {
+		this.lastThirdMonthRewardPoints = lastThirdMonthRewardPoints;
+	}
+
+	public long getTotalRewards() {
+		return totalRewards;
+	}
+
+	public void setTotalRewards(long totalRewards) {
+		this.totalRewards = totalRewards;
+	}
+
+}
